3c618112f4aacb8e8517bcac2be5dad1f91ece3e
Cris Arco <cris.arco83@gmail.com>
2022-08-16 22:00:54 +0200

    Iteración 1 pte

---
+++ vue-basics-files/src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="footer">
+    <ul>
+      <li>Ruta1</li>
+      <li>Ruta2</li>
+      <li>Ruta3</li>
+    </ul>
+  </div>
+</template>
+
+<style>
+.footer {
+    width: 100%;
+    display:flex;
+    justify-content: space-around;
+    align-items: center;
+}
+</style>
---
+++ vue-basics-files/src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="barra-nav">
+    <ul>
+      <li>Ruta1</li>
+      <li>Ruta2</li>
+      <li>Ruta3</li>
+    </ul>
+  </div>
+</template>
+
+<style>
+.barra-nav {
+    width: 100%;
+    display:flex;
+    justify-content: space-around;
+    align-items: center;
+}
+</style>
