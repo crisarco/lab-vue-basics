44363b163fc447ef8e6320f29156815e36a2bb67
Cris Arco <cris.arco83@gmail.com>
2022-08-30 21:18:21 +0200

    lab acabado

--- vue-basics-files/src/components/MyNewComponent.vue
+++ vue-basics-files/src/components/MyNewComponent.vue
@@ -1,5 +1,10 @@
 <template>
   <h1>{{ myString('Cris') }}</h1>
+  <br>
+  <div v-for='project in projects' :key='project.id'>
+    <h3>{{ project.title }}</h3>
+    <p>{{ project.description }}</p>
+  </div>
 </template>
 
 <script>
@@ -9,6 +14,27 @@ export default {
     myString(nombre) {
       return `Este es el nuevo componente de ${nombre}`;
     },
+  },
+  data() {
+    return {
+      projects: [
+        {
+          id: 0,
+          title: 'Project 1',
+          description: 'Esta es la descripción del proyecto1',
+        },
+        {
+          id: 1,
+          title: 'Project 2',
+          description: 'Esta es la descripción del proyecto2',
+        },
+        {
+          id: 2,
+          title: 'Project 3',
+          description: 'Esta es la descripción del proyecto3',
+        },
+      ],
+    };
   },
 };
 </script>
